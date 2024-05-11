7970fb0ebe97fc8370ea47731fd354432f443c4a
vlad13376 <245106@edu.p.lodz.pl>
2024-05-11 12:26:19 +0200

    Punkty 6,7,8 w zadaniu "Stałe i annotacje"

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,10 +1,12 @@
 package edu.kis.vh.nursery;
-
 public class DefaultCountingOutRhymer {
+    public static final int ARRAY_SIZE = 12;
+    public static final int DEFAULT_NEGATIVE_VALUE = -1;
+    public static final int ARRAY_SIZE_PLUS_DEFAULT_NEGATIVE_VALUE = 11;
 
-    private int[] numbers = new int[12];
+    private int[] numbers = new int[ARRAY_SIZE];
 
-    public int total = -1;
+    public int total = DEFAULT_NEGATIVE_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +14,22 @@ public class DefaultCountingOutRhymer {
     }
 
         public boolean callCheck() {
-            return total == -1;
+            return total == DEFAULT_NEGATIVE_VALUE;
         }
         
             public boolean isFull() {
-                return total == 11;
+                return total == ARRAY_SIZE_PLUS_DEFAULT_NEGATIVE_VALUE;
             }
         
                 protected int peekaboo() {
                     if (callCheck())
-                        return -1;
+                        return DEFAULT_NEGATIVE_VALUE;
                     return numbers[total];
                 }
             
                     public int countOut() {
                         if (callCheck())
-                            return -1;
+                            return DEFAULT_NEGATIVE_VALUE;
                         return numbers[total--];
                     }
 
